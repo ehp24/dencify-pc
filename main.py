065ef363c9088e3bea36e2826602682a34c46af5
ehp24 <ep590@cam.ac.uk>
2024-03-07 00:50:46 +0000

    more added comments and docstring

--- main.py
+++ main.py
@@ -169,31 +169,47 @@ def intrinsicsMat(focal_length_mm):
     
 def projection_WCS2PCS(intrinsicsMat:np.ndarray, extrinsicsMat:np.ndarray, points: np.ndarray,
                    map_width, map_height, path2las):
-    
-    las = laspy.read(path2las)
+    """Creates rgbd depth map and LAS point map of LAS points that coincide with the images pixels.
+
+    Args:
+        intrinsicsMat (numpy.ndarray): The camera intrinsics matrix.
+        extrinsicsMat (numpy.ndarray): The extrinsics matrix.
+        points (numpy.ndarray): The LAS points with columns being each point, rows being x,y,z,1,r,g,b,greyscale.
+        map_width (_type_): The width of map (img) in pixels.
+        map_height (_type_): The height of map in pixels.
+        path2las (str): The path to LAS file.
+
+    Returns:
+        numpy.ndarray: rgbd_map, LAS_map and projected img.
+    """
+    las = laspy.read(path2las) # so that we dont have to read las twice, we should do the reading in main script then pas in the la sobject instead
     x_offset, y_offset, z_offset = las.header.offsets
     x_scale, y_scale, z_scale = las.header.scales
     
     "depth_map = np.zeros(shape=(map_height, map_width), dtype=np.uint16)"
     
     rgbd_map = np.zeros(shape=(map_height, map_width,4), dtype=np.uint8) # each element at coord x,y has array [r,g,b,depth from cam cneter] with rgb in uint8 (normal), is rgb image wtih depth value for each coloured pixel
-    LAS_data_map = np.zeros(shape=(map_height, map_width,6), dtype='uint32') # stores [x_wc,y_wc,z_wc,r_wc_uint32,g_wc_uint32,b_wc_uint32] so all xyz and rgb of real world points but in LAS format that are captured in img
+    LAS_data_map = np.zeros(shape=(map_height, map_width,6), dtype='uint32') # stores [x_wc,y_wc,z_wc,r_wc_uint16,g_wc_uint16,b_wc_uint16] so all xyz and rgb of real world points but in LAS format that are captured in img
     LAS_new = []
-    points_ccs_hg = np.linalg.inv(extrinsicsMat) @ points[0:4, :]
-    points_pix_cs_hg = intrinsicsMat @  points_ccs_hg # converts homogeneous world points into homogeneous pixel coord points, inv exmat because need wcs rel2 ccs, but exmat is ccs rel2 wcs
     
+    # convert points from WCS to pixel coord system, each column is a uniwque point
+    points_ccs_hg = np.linalg.inv(extrinsicsMat) @ points[0:4, :] # rows x,y,z,1 and then every col i.e. every point in las pc. We now have homogeneuos coords in camera coord system, not world coord system
+    points_pix_cs_hg = intrinsicsMat @  points_ccs_hg # converts homogeneous world points CCS into homogeneous pixel coord points, inv exmat because need wcs rel2 ccs, but exmat is ccs rel2 wcs
     uv_rows = points_pix_cs_hg[0:2, :].reshape((2, points.shape[1])) # just takes the first two rows of projected_pts_homo (x/s, y/s) and makes new matrix
     w_row = points_pix_cs_hg[2, :].reshape((1, points.shape[1])) # creates 1D row vector of the s values (homogeneous last row of pix cord sustem points)
-    w_points = uv_rows / w_row # all LAS points in pixel coord system (non homo) (2 x N array)
+    w_points = uv_rows / w_row # all LAS points in pixel coord system (non homogeneous) (2 x N array)
 
     for pt_pix, pt_cam, pt_wcs in zip(w_points.transpose(),
                                    points_ccs_hg.transpose(),
                                    points.transpose()):
+        # this for loop goes row by row, i.e. each points data by point 
         
+        # maybe here further disect u,v = ptpix etc
         if 0 < pt_pix[0] < map_width and 0 < pt_pix[1] < map_height and pt_cam[2]>=0 : # only keep projected points that are within the actual map boundary, and if Z_ccs >0 (so we only get points INFORNT OF CAMERA)
             
             discretised_pt_pix = (int(pt_pix[0]), int(pt_pix[1])) # we cannot have decimals of pixels, so turn (x_proj_pixcs, y_proj_pixcs)  into ints 
             
+            # now we know which points are in our image boundary, we now want to create a densified pc
             # need to convert the valid points back into LAS compatible format (uint32)
             x_wc_uint32 = (pt_wcs[0] - x_offset)/x_scale
             y_wc_uint32 = (pt_wcs[1] - y_offset)/y_scale
@@ -209,22 +225,23 @@ def projection_WCS2PCS(intrinsicsMat:np.ndarray, extrinsicsMat:np.ndarray, point
             g_wc_uint16 = g_wc_uint8*256
             r_wc_uint16 = r_wc_uint8*256
             
-            """        
-            existing_depth_val = depth_map[discretised_pt_pix[1], discretised_pt_pix[0]]
-            depth_val = depth_factor * np.sqrt(np.sum(np.square(pt_cam))) 
+            #  old code
+            # existing_depth_val = depth_map[discretised_pt_pix[1], discretised_pt_pix[0]]
+            # depth_val = depth_factor * np.sqrt(np.sum(np.square(pt_cam))) 
         
-            if existing_depth_val == 0:
-                depth_map[discretised_pt_pix[1], discretised_pt_pix[0]] = depth_val
-            else:
-                depth_map[discretised_pt_pix[1], discretised_pt_pix[0]] = (existing_depth_val+depth_val)/2
-            """
+            # if existing_depth_val == 0:
+            #     depth_map[discretised_pt_pix[1], discretised_pt_pix[0]] = depth_val
+            # else:
+            #     depth_map[discretised_pt_pix[1], discretised_pt_pix[0]] = (existing_depth_val+depth_val)/2
+            
 
             # Colour depth map (rgbd) - projected points onto black plain image with the correct colours and distance from cam center 
             existing_depth = rgbd_map[discretised_pt_pix[1], discretised_pt_pix[0]][3]
-            depth = np.sqrt(np.sum(np.square(pt_cam[:3]))) # depth from the camera centre, as these pts are defined in cam coord system
+            depth = np.sqrt(np.sum(np.square(pt_cam[:3]))) # depth from the camera centre, as these pts are defined in cam coord system. square x,y,z, sum them then sqrt
             
+            # these two do the excat same thing if else so combine them!
             if existing_depth == 0:
-                LAS_new.append([r_wc_uint8,g_wc_uint8,b_wc_uint8,depth])
+                LAS_new.append([r_wc_uint8,g_wc_uint8,b_wc_uint8,depth]) # REDUNDANT?
                 rgbd_map[discretised_pt_pix[1], discretised_pt_pix[0]] = [r_wc_uint8,g_wc_uint8,b_wc_uint8,depth] # if using pil - RGB in arrays
                 LAS_data_map[discretised_pt_pix[1], discretised_pt_pix[0]] = [x_wc_uint32,y_wc_uint32,z_wc_uint32,r_wc_uint16,g_wc_uint16,b_wc_uint16]
 
@@ -234,7 +251,9 @@ def projection_WCS2PCS(intrinsicsMat:np.ndarray, extrinsicsMat:np.ndarray, point
                 LAS_data_map[discretised_pt_pix[1], discretised_pt_pix[0]] = [x_wc_uint32,y_wc_uint32,z_wc_uint32,r_wc_uint16,g_wc_uint16,b_wc_uint16]
             
     projected_img = rgbd_map[:,:,:3] # same as rgbd_map, but removed depth value from all the arrays, so just an image
-    
+    # LAS_data_map : at every u,v of the u*v 2D map, there is a third array that conatins [x_wc,y_wc,z_wc,r_wc,g_wc,b_wc] so every correpsonging LAS world point that coincides at this pixel point
+    # rgbd map : every las point's rgb + depth data in uint8 so visisble, located as a array at every [u,v] pixel coord on map 
+    # projected_img : same as rgbd just rempve depth so we have a visible image
     return rgbd_map, projected_img, LAS_data_map
 
 def interpolateImage(): 
